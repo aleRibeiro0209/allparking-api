d2f8647928c5c0199002aece68b10bea00fbe3be
Alexandre Ribeiro <alexandreribeiro0209@gmail.com>
2024-07-16 01:39:18 -0300

    Update - index.php for date_time zone

--- public/index.php
+++ public/index.php
@@ -4,5 +4,8 @@ use App\Core\Router;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+    date_default_timezone_set("America/Sao_Paulo");
+
     header('Content-type: application/json');
+    
     new Router();
